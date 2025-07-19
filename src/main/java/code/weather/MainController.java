acb146c2eefcb86ac317fc7ed695bbf753c98e6f
Anh Nguyen <anh.nguyen.huymai@gmail.com>
2025-07-18 18:54:19 -0600

    Setting up a basic GUI

--- src/main/java/code/weather/MainController.java
+++ src/main/java/code/weather/MainController.java
@@ -2,13 +2,48 @@ package code.weather;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView imageForHumidity;
+
+    @FXML
+    private ImageView imageForSearching;
+
+    @FXML
+    private ImageView imageForWeather;
+
+    @FXML
+    private ImageView imageForWindSpeed;
+
+    @FXML
+    private Label labelForDate;
+
+    @FXML
+    private Label labelForHumidity;
+
+    @FXML
+    private Label labelForTemperature;
+
+    @FXML
+    private Label labelForWindSpeed;
+
+    @FXML
+    private TextField textFieldForArea;
+
+    @FXML
+    void searchImageTrigger(MouseEvent event) {
+
     }
+
+    @FXML
+    void initialize(){
+//        imageForHumidity.setImage();
+    }
+
 }
